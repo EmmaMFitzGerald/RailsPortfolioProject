765e1844797eece516332aa7e7ba6195733e26d5
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-10-14 13:45:33 -0400

    omniauth working

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -283,6 +283,7 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],  scope: 'email', info_fields: 'email, first_name, last_name'
 
   # ==> Turbolinks configuration
   # If your app is using Turbolinks, Turbolinks::Controller needs to be included to make redirection work correctly:
@@ -296,4 +297,7 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  
+
+
 end
