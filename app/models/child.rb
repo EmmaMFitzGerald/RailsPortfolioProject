ea98e373fdca38a6acdcfc466ee6baca77b57a01
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-10-18 14:20:46 -0400

    added age to childrens index

--- app/models/child.rb
+++ app/models/child.rb
@@ -9,4 +9,8 @@ class Child < ApplicationRecord
 
     scope :search, -> (parameter) {where("lower(name) LIKE :search", search: "%#{parameter}%")   }
 
+    def age(birthday)
+        now = Time.now.utc.to_date
+        now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+    end
 end  
